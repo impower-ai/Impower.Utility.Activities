6c24ce46176e32e51b9c1c9fdd15269494d411a6
Matthew Jackson <103076064+matthewjackson202@users.noreply.github.com>
2022-07-29 23:58:09 -0400

    Stash

---
+++ Impower.RingCentral/Models/CleanedDictionary.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Impower.Queue.Activities.Utilities;
+
+namespace Impower.Queue.Activities.Models
+{
+    public class CleanedDictionary : IDictionary
+    {
+
+        private dynamic ConvertedDictionary { get; set; }
+
+        public CleanedDictionary(Dictionary<string, object> specificContent, Type dictionaryType)
+        {
+            
+            switch (Type.GetTypeCode(dictionaryType))
+            {
+                case TypeCode.Int32:
+                    this.ConvertedDictionary = Util.ConvertedDictionary<int>(specificContent);
+                    break;
+                case TypeCode.String:
+                    this.ConvertedDictionary = Util.ConvertedDictionary<string>(specificContent);
+                    Console.WriteLine(this.ConvertedDictionary);
+                    break;
+                default:
+                    throw new Exception("Could not parse provided type. Please contact Matthew.");
+            }
+        }
+
+
+        public object this[object key] { get => ConvertedDictionary[key]; set => global::System.Console.WriteLine(value); }
+
+        public ICollection Keys => ConvertedDictionary.Keys.ToArray();
+
+        public ICollection Values => ConvertedDictionary.Values.ToArray();
+
+        public bool IsReadOnly => false;
+
+        public bool IsFixedSize => false;
+
+        public int Count => ConvertedDictionary.Count;
+
+        public object SyncRoot => throw new NotImplementedException();
+
+        public bool IsSynchronized => throw new NotImplementedException();
+
+        public void Add(object key, object value)
+        {
+            ConvertedDictionary.Add(key, value);
+        }
+
+        public void Clear()
+        {
+            ConvertedDictionary = null;
+        }
+
+        public bool Contains(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionaryEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(object key)
+        {
+            ConvertedDictionary?.Remove(key);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Impower.RingCentral/Utilities/Util.cs
+++ Impower.RingCentral/Utilities/Util.cs
@@ -11,19 +11,17 @@ namespace Impower.Queue.Activities.Utilities
         public static Dictionary<String, T> ConvertedDictionary<T>(Dictionary<String, Object> specificContent)
         {
             if (specificContent == null) return null;
+            Console.WriteLine("has content");
             var RunningDictionary = new Dictionary<String, T>();
             foreach (KeyValuePair<String, Object> pair in specificContent)
             {
-                Console.WriteLine(typeof(T));
                 if (pair.Value == null) continue;
 
                 switch (Type.GetTypeCode(typeof(T)))
                 {
                     case TypeCode.Int32:
-                        if (Int32.TryParse(pair.Value.ToString(), out var parsed))
-                        {
-                            RunningDictionary.Add(pair.Key, (T)(object)parsed);
-                        }
+                        var ableToParse = Int32.TryParse(pair.Value.ToString(), out var parsed);
+                        RunningDictionary.Add(pair.Key, ableToParse ? (T)(object)parsed : (T)(object)null);
                         break;
                     case TypeCode.String:
                         if (String.IsNullOrEmpty(pair.Value.ToString())) { continue; }
