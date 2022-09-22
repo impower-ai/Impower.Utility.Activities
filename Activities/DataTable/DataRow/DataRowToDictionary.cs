fb82f2d1e2012d9fa7dfc7929719a83507146f2e
Matthew Jackson <103076064+matthewjackson202@users.noreply.github.com>
2022-09-22 15:32:58 -0400

    Add DataTable activity pack with documentation

---
+++ Activities/DataTable/DataRow/DataRowToDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Activities;
+using System.ComponentModel;
+using Impower.Utility.Utilities;
+using System.Linq;
+using Impower.Utility.Activities;
+using System.Data;
+
+namespace Impower.Utility.DataTable
+{
+    public class DataRowToDictionary : UtilityActivity
+    {
+
+        [Category("Input")]
+        [RequiredArgument]
+        [DisplayName("DataRow to Convert")]
+        public InArgument<DataRow> DataRowInput { get; set; }
+
+
+        [Category("Output")]
+        [DisplayName("Created Dictionary")]
+        public OutArgument<Dictionary<string, object>> Dictionary { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var dataRow = DataRowInput.Get(context);
+
+            var dictionary = dataRow.ToDictionary();
+
+            Dictionary.Set(context, dictionary);
+
+        }
+    }
+}
---
+++ Activities/DataTable/DataRow/DataRowToTypedDictionary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Activities;
+using Impower.Utility.Utilities;
+using System.ComponentModel;
+using Impower.Utility.Models;
+using System.Data;
+
+namespace Impower.Utility.DataTable
+{
+    public class DataRowToTypedDictionary<T> : TypedDictionaryActivity<T>
+    {
+        public override OutArgument<Dictionary<string, T>> Dictionary { get; set; }
+        public override InArgument<T> DefaultValue { get; set; }
+
+        [RequiredArgument]
+        [Category("Input")]
+        [DisplayName("DataRowInput")]
+        public InArgument<DataRow> DataRowInput { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+
+            var defaultValue = DefaultValue.Get(context);
+            var dataRow = DataRowInput.Get(context);
+
+            Dictionary.Set(context, dataRow.ToTypedDictionary<T>(defaultValue));
+
+        }
+    }
+}
